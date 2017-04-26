44d327641965f8a920a3b29f2634d459128c0f65
kianadk <kianadkavoosi@gmail.com>
2017-04-25 17:02:00 -0700

    skeleton code and static hello world

---
+++ cs130-shwipe/src/main/java/edu/ucla/cs130/shwipe/Cs130ShwipeApplication.java
@@ -0,0 +1,12 @@
+package edu.ucla.cs130.shwipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs130ShwipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cs130ShwipeApplication.class, args);
+	}
+}
---
+++ cs130-shwipe/src/test/java/edu/ucla/cs130/shwipe/Cs130ShwipeApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.ucla.cs130.shwipe;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Cs130ShwipeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
